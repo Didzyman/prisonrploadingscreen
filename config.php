ae695dd795e6ae749b1d1c0270c1396e277d9948
Didzyman <30914406+Didzyman@users.noreply.github.com>
2018-02-10 19:26:36 +0000

    start

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+
+ /* An XML database can easily be read, therefore, private information should be set here */
+	
+	$user = "byron"; // username
+	$passw = "kraken"; // password
+	$apikey = "0DDFC76ECA6D943624323AFDFFA38DA8"; // Find your API Key at https://steamcommunity.com/dev/apikey
+
+?>
